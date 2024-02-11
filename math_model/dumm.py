ba0bf2d71a4bd316ffff7b5f5a36133675719057
Faris Durrani <afarisdurrani@gmail.com>
2024-02-11 01:45:25 -0500

    added best multiprocess to streamlit commented

--- math_model/dumm.py
+++ math_model/dumm.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pydeck as pdk
-import streamlit as st
-from multiprocess import Pool
+from multiprocessing import Pool
+
+# import streamlit as st
 
 GEONAME_ID = "metro"
 NAME = "metro"
@@ -14,60 +15,6 @@ COU_NAME_EN = "country_code"
 LATITUDE = "latitude"
 LONGITUDE = "longitude"
 DATASET_CSV_PATH = "../data/metro_regions.csv"
-
-
-def draw_all(dataset, dataset_statistics):
-    # Text box input
-    user_city_input = st.text_input("Enter the origin city:", "Atlanta")
-    user_A_input = st.slider(
-        "How important is the number of people connected?",
-        min_value=0.0,
-        max_value=5.0,
-        step=0.1,
-        value=1.0,
-    )
-    user_B_input = st.slider(
-        "How important is a short distance?",
-        min_value=0.0,
-        max_value=5.0,
-        step=0.1,
-        value=1.0,
-    )
-    path_df = get_viable_cities_paths(
-        user_city_input,
-        dataset,
-        dataset_statistics,
-        5,
-        A=float(user_A_input),
-        B=float(user_B_input),
-    )
-
-    st.dataframe(path_df)
-
-    def hex_to_rgb(h):
-        h = h.lstrip("#")
-        return tuple(int(h[i : i + 2], 16) for i in (0, 2, 4))
-
-    path_df["color"] = path_df["color"].apply(hex_to_rgb)
-
-    view_state = pdk.ViewState(latitude=37.782556, longitude=-122.3484867, zoom=2)
-
-    layer = pdk.Layer(
-        type="PathLayer",
-        data=path_df,
-        pickable=True,
-        get_color="color",
-        width_scale=20,
-        width_min_pixels=2,
-        get_path="path",
-        get_width=5,
-    )
-
-    r = pdk.Deck(
-        layers=[layer], initial_view_state=view_state, tooltip={"text": "{name}"}
-    )
-
-    st.pydeck_chart(r)
 
 
 def get_dataset(csv_name):
@@ -120,6 +67,8 @@ def get_city_item(name_or_id, dataset):
         subset = dataset[dataset[GEONAME_ID] == name_or_id]
     else:
         subset = dataset[dataset[NAME].str.contains(name_or_id) == True]
+    if len(subset) == 0:
+        raise Exception(f"City {name_or_id} not found")
     return get_city_with_max_pop(subset)
 
 
@@ -180,12 +129,12 @@ def get_viable_cities_paths(
         city_2 = city[1]
         paths_dict.append(
             {
-                "city_2": city_2[NAME],
-                "viability": f"{city[0] * 100}%",
                 "path": [
                     [city_1[LONGITUDE], city_1[LATITUDE]],
                     [city_2[LONGITUDE], city_2[LATITUDE]],
                 ],
+                "viability": f"{city[0] * 100}%",
+                "city_2": city_2[NAME],
             }
         )
     red = Color("red")
@@ -195,39 +144,54 @@ def get_viable_cities_paths(
     return pd.DataFrame(paths_dict)
 
 
-def get_most_viable_data(data_bundle):
-    city_item, dataset, dataset_statistics = (
-        data_bundle["city_item"],
-        data_bundle["dataset"],
-        data_bundle["dataset_statistics"],
-    )
+dataset = get_dataset(DATASET_CSV_PATH)
+
+# remove empty rows
+dataset = dataset.dropna()
+print(len(dataset))
+
+# dataset = dataset[dataset[COU_NAME_EN] == "United States"]
+dataset_statistics = {
+    "min_pop": dataset[POPULATION].min(),
+    "max_pop": dataset[POPULATION].max(),
+    "avg_pop": dataset[POPULATION].mean(),
+    "min_dist": 0,
+    "max_dist": get_max_dist(get_city_item("Atlanta", dataset), dataset),
+}
+
+# Text box input
+user_city_input = "Atlanta"
+user_A_input = 1
+user_B_input = 1
+df1 = get_viable_cities_paths(
+    user_city_input,
+    dataset,
+    dataset_statistics,
+    5,
+    A=float(user_A_input),
+    B=float(user_B_input),
+)
+
+
+def get_most_viable_data(city_item):
+    city = city_item
     max_viable_cities = find_top_viable_cities(
-        city_item[NAME], dataset, dataset_statistics=dataset_statistics
+        city[NAME], dataset, dataset_statistics=dataset_statistics
     )
-    return {"source": city_item, "max_viable_cities": max_viable_cities}
+    return {"source": city, "max_viable_cities": max_viable_cities}
 
 
-def get_most_viable_for_all(dataset, dataset_statistics):
+def get_most_viable_for_all(dataset):
     max_viable_cities = []
     with Pool(4) as p:
-        max_viable_cities = p.map(
-            get_most_viable_data,
-            (
-                {
-                    "city_item": row,
-                    "dataset": dataset,
-                    "dataset_statistics": dataset_statistics,
-                }
-                for _, row in dataset.iterrows()
-            ),
-        )
+        max_viable_cities = p.map(get_most_viable_data, (row for _, row in dataset.iterrows()))
     # for _, city_item in tqdm(dataset.iterrows(), total=len(dataset)):
     #     max_viable_cities.append(get_most_viable_data(city_item))
     return max_viable_cities
 
 
-def get_best_of_best(dataset, dataset_statistics, top_n=5):
-    max_viable_cities = get_most_viable_for_all(dataset, dataset_statistics)
+def get_best_of_best(dataset, top_n=5):
+    max_viable_cities = get_most_viable_for_all(dataset)
     max_viable_cities_list = []
     for city_source in max_viable_cities:
         city_source_item = city_source["source"]
@@ -250,25 +214,6 @@ def get_best_of_best(dataset, dataset_statistics, top_n=5):
     best_of_best = max_viable_cities_df.head(top_n)
     return best_of_best
 
-
 if __name__ == "__main__":
-    dataset = get_dataset(DATASET_CSV_PATH)
-    dataset_statistics = {
-        "min_pop": dataset[POPULATION].min(),
-        "max_pop": dataset[POPULATION].max(),
-        "avg_pop": dataset[POPULATION].mean(),
-        "min_dist": 0,
-        "max_dist": get_max_dist(get_city_item("Atlanta", dataset), dataset),
-    }
-
-    # bb = get_best_of_best(dataset, dataset_statistics, 5)
-    # print(bb)
-
-    tab1, tab2 = st.tabs(["Math Model", "DL Model"])
-
-    with tab1:
-        st.title("Math Model")
-        draw_all(dataset, dataset_statistics)
-
-    with tab2:
-        st.title("DL Model")
+    bb = get_best_of_best(dataset, 5)
+    print(bb)
